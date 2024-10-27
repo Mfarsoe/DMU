e33cb73bba7f73c4e5fb5ff3a031c6a094537587
Mfarsoe <marfa01@eamv.dk>
2024-10-27 16:17:11 +0100

    Leetcode

---
+++ Leetcode/217. Contains Duplicate/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int nums[] = {1, 2, 3, 3};
+        Solution sol = new Solution();
+        sol.hasDuplicate(nums);
+    }
+}
