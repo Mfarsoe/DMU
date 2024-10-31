c19505ff9aebd4f216937a35c7987fcb8d6c4e10
Mfarsoe <marfa01@eamv.dk>
2024-10-31 14:23:09 +0100

    Update Main.java

--- 1.År/Uge 43/SysUdv Case - Libary/src/Main.java
+++ 1.År/Uge 43/SysUdv Case - Libary/src/Main.java
@@ -39,6 +39,8 @@ public class Main {
                             lib.getBooks();
                             break;
                     }
+                case 2:
+                    
             }
         }
     }
