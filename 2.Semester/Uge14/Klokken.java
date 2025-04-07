2385dd7b005656e9600a7a24c429ebaf8f8ef8d3
Mfarsoe <marfa01@eamv.dk>
2025-04-07 11:35:08 +0200

    a

---
+++ 2.Semester/Uge14/Klokken.java
@@ -0,0 +1,58 @@
+
+public class Klokken {
+  private static final int ET_DØGN = 24 * 60 * 60;
+
+  private int sekunder;
+
+  public Klokken() {
+    this(0, 0, 0);
+  }
+
+  public Klokken(Klokken other) {
+    this(other.sekunder);
+  }
+
+  public Klokken(int sekunder) {
+    this.sekunder = sekunder;
+  }
+
+  public Klokken(int timer, int minutter, int sekunder) {
+    this.sekunder = timer * 60 + minutter * 60 + sekunder;
+  }
+
+  public int getTimer() {
+    return sekunder % 3600;
+  }
+
+  public int getMinutter() {
+    return sekunder / 60;
+  }
+
+  public int getSekunder() {
+    return sekunder % 60;
+  }
+
+  public void add(Klokken other) {
+    this.sekunder += other.sekunder;
+
+    other.sekunder %= ET_DØGN;
+  }
+
+  public void sub(Klokken other) {
+    this.sekunder -= this.sekunder;
+
+    if (this.sekunder < 0)
+      this.sekunder += ET_DØGN;
+  }
+
+  public boolean equals(Object other) {
+    if (other instanceof Klokken)
+      return this.sekunder == ((Klokken) other).sekunder;
+    else
+      return false;
+  }
+
+  public String toString() {
+    return "[" + getTimer() + getMinutter() + ":" + getSekunder() + "]";
+  }
+}
---
+++ 2.Semester/Uge14/ThreadOpg1/ThreadOpg1/TennisSpiller.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadOpg1 {
+    class TennisSpiller {
+        private string name;
+        private bool harBold;
+        private TennisSpiller modstander;
+        Random rnd = new Random();
+        private Thread spillerThread;
+
+        public TennisSpiller(String name) {
+            this.name = name;
+
+            harBold = false;
+            spillerThread = new Thread(Run);
+        }
+
+        public void Start() {
+            spillerThread.Start();
+        }
+
+        public void Join() {
+            spillerThread.Join();
+        }
+
+        public void Run() {
+            while (true) {
+                while (!harBold)
+                    Thread.Sleep(1);
+
+                Thread.Sleep(rnd.Next(0, 2000));
+
+                if (rnd.Next(0, 100) > 70) {
+                    harBold = false;
+
+                    Console.WriteLine(name + " retunerer!");
+
+                    modstander.ModtagBold();
+                } else {
+                    Console.WriteLine(name + " fumler bolden!");
+                    break;
+
+                }
+            }
+
+        }
+        public void SetModstander(TennisSpiller modstander) {
+            this.modstander = modstander;
+        }
+
+        public void ModtagBold() {
+            harBold = true;
+        }
+    }
+}
