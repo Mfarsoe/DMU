11ec911ad7ca9eda2e274634c7b78e471679b58d
Mfarsoe <marfa01@eamv.dk>
2025-08-28 07:59:14 +0200

    a

---
+++ 3.Semester/Uge 35/AsymmetricEncryptionExample/AsymmetricEncryptionExample/RSAManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsymmetricEncryptionExample
+{
+    public class RSAManager
+    {
+        private RSACryptoServiceProvider myRSA;
+        private RSAParameters rsaParameters;
+        private RSACryptoServiceProvider otherRSA;
+
+
+        public RSAManager()
+        {
+            myRSA = new RSACryptoServiceProvider();
+            rsaParameters = myRSA.ExportParameters(true);
+        }
+
+        // Load Private Key from file
+        public void loadKeyMenuItem_Click(string filepath)
+        {
+            string xmlKeys = File.ReadAllText(filepath);
+            myRSA.FromXmlString(xmlKeys);
+        }
+
+        // Save Private Key to file
+        public void saveKeyMenuItem_Click(string filepath)
+        {
+            string xmlKeys = myRSA.ToXmlString(true);
+            File.WriteAllText(filepath, xmlKeys);
+        }
+
+        // Import other public key
+        public void importKeyMenuItem_Click(string filePath)
+        {
+            string xmlKey = File.ReadAllText(filePath);
+            otherRSA = new RSACryptoServiceProvider();
+            otherRSA.FromXmlString(xmlKey);
+        } 
+        //Export public key
+        public void exportKeyMenuItem_Click(string filepath)
+        {
+            string xmlKey = myRSA.ToXmlString(false);
+            File.WriteAllText(filepath, xmlKey);
+        }
+
+    }
+}
