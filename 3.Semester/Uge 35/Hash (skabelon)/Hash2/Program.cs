ffe9b94df3d9117ed5c897071080b46e8c4454be
Mfarsoe <marfa01@eamv.dk>
2025-08-29 11:13:41 +0200

    a

---
+++ 3.Semester/Uge 35/Hash (skabelon)/Hash2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Hash {
+
+  public class Program {
+
+    // hash -c -f "c:\test\testdata.txt"   // vil udskrive hash for filens indhold
+    // hash -v -f "c:\test\testdata.txt" -h d56d+5qwf/d5fr5   // check at fil har hashværdi
+
+    private static void Main(string[] args) {
+      string filename = null;
+      string hash = null;
+      bool? compute = null;
+
+      for (int i = 0; i < args.Length; i++)
+        switch (args[i]) {
+          case "-c":
+            compute = true;
+            break;
+
+          case "-v":
+            compute = false;
+            break;
+
+          case "-f":
+            if (i + 1 < args.Length)
+              filename = args[++i];
+            else
+              Console.WriteLine("Argument -f missing filename");
+            break;
+
+          case "-h":
+            if (i + 1 < args.Length)
+              hash = args[++i];
+            else
+              Console.WriteLine("Argument -h missing hash");
+            break;
+
+          default:
+            Console.WriteLine("Unexpected argument: " + args[i]);
+            break;
+        }
+
+      if (compute == true) {
+        // validate arguments
+        if (filename != null) {
+                    //TODO compute hash of filename with md5
+                    Console.WriteLine(ComputeHash(filename));
+                }
+                else
+          Console.WriteLine("Compute hash, missing filename");
+
+      } else if (compute == false) {
+        // validate arguments
+        if (filename != null) {
+          if (hash != null) {
+            //-> verify hash
+            if(ComputeHash(filename) == hash)
+              Console.WriteLine("Hash verified");
+            else
+              Console.WriteLine("Hash NOT verified");
+          } else
+            Console.WriteLine("Verify hash, missing hash");
+
+        } else
+          Console.WriteLine("Verify hash, missing filename");
+
+      } else // compute == null
+        Console.WriteLine("Missing -c or -v (i.e. compute or verify hash)");
+    }
+        private static string ComputeHash(string filename)
+        {
+            byte[] data = File.ReadAllBytes(filename);
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] hashed = md5.ComputeHash(data);
+            string base64Hash = Convert.ToBase64String(hashed);
+            return base64Hash;
+        }
+  }
+}
---
+++ 3.Semester/Uge 35/Opgave A/Opgave A/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+namespace Opgave_A
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person() { }
+    
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string ToXML() 
+        {
+            string xml;
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                using (XmlWriter xmlWriter = XmlWriter.Create(stringWriter))
+                {
+                    ToXml(xmlWriter);
+                    xmlWriter.Flush();
+                    xml = stringWriter.GetStringBuilder().ToString();
+                }
+            }
+            return xml;
+        }
+
+        public void FromXML(string xml) 
+        {
+            using (StringReader stringReader = new StringReader(xml))
+            {
+                using (XmlReader xmlReader = XmlReader.Create(stringReader))
+                {
+                    FromXml(xmlReader);
+                }
+            }
+        }
+
+        public void ToXml(XmlWriter writer) 
+        {
+            writer.WriteStartElement("Person");
+            writer.WriteElementString("Name", name);
+            writer.WriteElementString("Age", age.ToString());
+            writer.WriteEndElement();
+        }
+
+        public void FromXml(XmlReader reader) 
+        {
+            reader.ReadToFollowing("Person");
+            reader.ReadToFollowing("Name");
+            name = reader.ReadElementContentAsString();
+            reader.ReadToFollowing("Age");
+            age = reader.ReadElementContentAsInt();
+        }
+    } 
+}
