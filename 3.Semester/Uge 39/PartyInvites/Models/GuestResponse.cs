6d624c8e870f20ed5acc3075cbbe126a74a71742
Mfarsoe <marfa01@eamv.dk>
2025-09-23 09:51:05 +0200

    Address og Participants tilføjet

--- 3.Semester/Uge 39/PartyInvites/Models/GuestResponse.cs
+++ 3.Semester/Uge 39/PartyInvites/Models/GuestResponse.cs
@@ -8,9 +8,15 @@ namespace PartyInvites.Models
         public string? Name { get; set; }
         [Required(ErrorMessage = "Please enter your email address")]
         public string? Email { get; set; }
+        [Required(ErrorMessage = "Please enter your address")]
+        public string? Address { get; set; }
         [Required(ErrorMessage = "Please enter your phone number")]
         public string? Phone { get; set; }
         [Required(ErrorMessage = "Please specify whether you will attend")]
         public bool? WillAttend { get; set; }
+        [Required(ErrorMessage = "Please enter the number of participants")]
+        [Range(1, 20, ErrorMessage = "Please enter a number between 1 and 20")]
+        public int? Participants { get; set; }
+
     }
 }
