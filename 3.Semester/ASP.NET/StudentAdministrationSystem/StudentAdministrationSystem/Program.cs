8f44efeeff46a65dc8e3fafb2afe3dff4bbc3dfa
Mfarsoe <marfa01@eamv.dk>
2025-10-30 10:41:43 +0100

    StudentManagement Start på auth

--- 3.Semester/ASP.NET/StudentAdministrationSystem/StudentAdministrationSystem/Program.cs
+++ 3.Semester/ASP.NET/StudentAdministrationSystem/StudentAdministrationSystem/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using StudentAdministrationSystem.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
@@ -9,10 +10,21 @@ builder.Services.AddDbContext<StudentDbContext>(options => {
 });
 
 builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+
+
+builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+options.UseSqlServer(
+builder.Configuration["ConnectionStrings:IdentityConnection"]));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+.AddEntityFrameworkStores<AppIdentityDbContext>();
+
 var app = builder.Build();
 app.UseStaticFiles();
 app.UseRouting();
 app.MapDefaultControllerRoute();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "pagination",
@@ -25,4 +37,5 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}"
 );
 SeedData.EnsurePopulated(app);
+IdentitySeedData.EnsurePopulated(app);
 app.Run();
--- 3.Semester/ASP.NET/StudentAdministrationSystem/StudentAdministrationSystem/obj/Debug/net6.0/StudentAdministrationSystem.AssemblyInfo.cs
+++ 3.Semester/ASP.NET/StudentAdministrationSystem/StudentAdministrationSystem/obj/Debug/net6.0/StudentAdministrationSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentAdministrationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+987fbe6e2fae7353a2fa8e8775514bb722f0a0fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e96035c724a18ba543dd290318f475921b027c67")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentAdministrationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentAdministrationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
