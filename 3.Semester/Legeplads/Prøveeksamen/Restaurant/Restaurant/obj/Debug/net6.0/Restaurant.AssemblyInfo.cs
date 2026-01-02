77805571d00bdb22ed565030e562bbfa984512d2
Mfarsoe <marfa01@eamv.dk>
2026-01-02 15:51:31 +0100

    a

--- 3.Semester/Legeplads/Prøveeksamen/Restaurant/Restaurant/obj/Debug/net6.0/Restaurant.AssemblyInfo.cs
+++ 3.Semester/Legeplads/Prøveeksamen/Restaurant/Restaurant/obj/Debug/net6.0/Restaurant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f919a318749ed1aaf35f1f3c21c99cbc5930fb29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98ff486641e2a9fda2c46b30af04ee01e10a5340")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
