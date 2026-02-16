b4ff289a940696d8ceaa258e2606b57bd3a63690
Mfarsoe <marfa01@eamv.dk>
2026-02-16 21:12:41 +0100

    Øvelser

---
+++ 4.Semester/WebPro/Uge 6/Øvelser/Øvelse2/script.js
@@ -0,0 +1,27 @@
+// Her skal du skrive din JavaScript kode
+
+// Referencer til links ('Less' og 'More') og containeren til tekstfelterne
+var less = document.getElementById('less'); 
+var more = document.getElementById('more');
+var container = document.getElementById('container');
+
+// Event listeners til 'Less' og 'More'
+more.addEventListener('click', addTextField);
+less.addEventListener('click', removeTextField);
+
+// Tilføjer et tekstfelt (max 5)
+function addTextField() {
+    var textFields = container.getElementsByTagName('input').length;
+    if (textFields < 5) {
+        var newInput = document.createElement('input');
+        newInput.type = 'text';
+        container.appendChild(newInput);
+    }
+}
+// Fjerner et tekstfelt (min 0)
+function removeTextField() {
+    var textFields = container.getElementsByTagName('input');
+    if (textFields.length > 0) {
+        container.removeChild(textFields[textFields.length - 1]);
+    }
+}
